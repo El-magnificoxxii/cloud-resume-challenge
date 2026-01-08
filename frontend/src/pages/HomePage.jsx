ad281372438df53131190df6a9b5ab75f14a30fa
El-magnificoxxii <75856679+El-magnificoxxii@users.noreply.github.com>
2026-01-08 22:06:14 +0000

    implement view counter and mock api

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,19 +3,21 @@ import 'css/pages/home.css'
 import abdullateef_oni from 'images/abdullateef-oni-thumb.webp'
 import blogData from 'data/blogData.json'
 import linksData from 'data/linksData.json'
-import PostItem from "comps/PostItem"
+import PostItem from 'comps/PostItem'
+import ViewCounter from 'comps/ViewCounter'
 
 export default function HomePage() {
   const sortedBlogData = [...blogData].sort((a, b) => new Date(b.date) - new Date(a.date));
   return (
     <>
         <h1 className='fancy'>Abdullateef Oni's Blog</h1>
+        <ViewCounter />
         <div className="intro_video">
           <img src={abdullateef_oni} />
         </div>
         <div className='links'>
           {linksData.map((link) => (
-            <a target="_blank" href={link.url}>
+            <a key={link.url} target="_blank" href={link.url}>
               <span className="icon" dangerouslySetInnerHTML={{ __html: link.icon }} />
               <span className="name">{link.name}</span>
             </a>
