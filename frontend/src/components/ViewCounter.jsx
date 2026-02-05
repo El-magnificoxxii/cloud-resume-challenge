786ba5450d6e4d25af947ab199cba0477398ae85
El-magnificoxxii <75856679+El-magnificoxxii@users.noreply.github.com>
2026-02-05 21:55:48 +0100

    adjusted counter to count properly after reloading webpage

--- frontend/src/components/ViewCounter.jsx
+++ frontend/src/components/ViewCounter.jsx
@@ -4,11 +4,12 @@ export default function ViewCounter() {
   const [count, setCount] = useState(0);
   const endpoint = import.meta.env.VITE_COUNTER_ENDPOINT;
   useEffect(() => {
-    fetch(endpoint)
-      .then((res) => res.json())
-      .then((data) => setCount(Number(data.count) || 0))
-      .catch((err) => console.error("Error fetching count:", err));
-  }, []);
+  fetch(endpoint, { method: "POST" })
+    .then((res) => res.json())
+    .then((data) => setCount(Number(data.count) || 0))
+    .catch((err) => console.error("Error incrementing count:", err));
+}, []);
+
 
   const increment = () => {
     fetch(endpoint, { method: "POST" })
